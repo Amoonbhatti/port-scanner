f054f9c91936268737471347f2ca66355bbb524e
AmoonBhatti <amoonsaab@gmail.com>
2026-03-21 00:04:34 +0500

    Added port scanner tool

--- port_scanner.py
+++ port_scanner.py
@@ -1,10 +1,12 @@
 import socket
 
 target = input("Enter target (IP or domain): ")
+start_port = int(input("Enter start port: "))
+end_port = int(input("Enter end port: "))
 
-print(f"\nScanning {target}...\n")
+print(f"\nScanning {target} from port {start_port} to {end_port}...\n")
 
-for port in range(20, 101):
+for port in range(start_port, end_port + 1):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket.setdefaulttimeout(0.5)
 
